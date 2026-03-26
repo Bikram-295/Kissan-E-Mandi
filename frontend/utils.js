f61ca25ee8b6c08e50c4827cca2fe17eb5a517ac
Bikram-295 <rajhiron1981@gmail.com>
2026-03-26 14:43:10 +0530

    Prepare frontend for web deployment

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import SharedPreferences from 'react-native-shared-preferences'
+
 
 
 export const checkLoggedIn = async (navigator) => {
